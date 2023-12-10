fdbb8a627272a3fb287ac1f67344b7beac985c34
nechama <nechamy5834@gmail.com>
2023-12-11 00:14:15 +0200

    add fs

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('remove all');
+var fs = require('fs');
+var path = require('path');
+var dir = path.join(__dirname, 'tmp');
+fs.readdir(dir, function(err, files) {
+  if (err) throw err;
+  for (var i = 0; i < files.length; i++) {
+    fs.unlink(path.join(dir, files[i]), function(err) {
+      if (err) throw err;
+      console.log('successfully deleted');
+    });
+  }
+});
