ace122ffe6c9520d579a10200e1d5fb71fd51146
nechama <nechamy5834@gmail.com>
2023-12-13 09:17:07 +0200

    .\objects.js

---
+++ objects.js
@@ -0,0 +1,20 @@
+//אוביקט של עסק
+const business = {
+    name: "Coding Academy",
+    address: "Rothschild 60 Tel Aviv",
+    phone: "03-1234567",
+    owner: "Yariv Katz",
+    logo: "https://coding-academy.org/images/ca_logo.png",
+    description: "The best coding academy in the world",
+};
+
+//אוביקט של פגישה
+const meeting = {
+    serviceName: "פגישת ייעוץ",
+    serviceDescription: "פגישת ייעוץ עם יריב כץ",
+    servicePrice: 500,
+    dateTime: "2021-06-20T10:00:00.000Z",
+    clientName: "אבי כהן",
+    clientPhone: "050-1234567",
+    clientEmail: "m@m.com",
+};
